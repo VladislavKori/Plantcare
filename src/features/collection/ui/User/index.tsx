436b84912cf03817a0e5c3235e3ed0ea6a2c1138
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-03 21:03:23 +0500

    feat(user): refactor User component

--- src/features/collection/ui/User/index.tsx
+++ src/features/collection/ui/User/index.tsx
@@ -2,17 +2,18 @@ import { FC } from "react";
 import styles from "./style.module.scss";
 
 export const User: FC = () => {
+    const photoURL = window.Telegram.WebApp.initDataUnsafe?.user?.photo_url
+    const name = window.Telegram.WebApp.initDataUnsafe?.user?.first_name;
+    const username = window.Telegram.WebApp.initDataUnsafe?.user?.username;
+
     return (
         <div className={styles["user"]}>
             <div className={styles["user-avatar"]}>
-                <img 
-                    src={window.Telegram.WebApp.initDataUnsafe?.user?.photo_url}
-                    alt="user" 
-                />
+                <img src={photoURL} alt="user" />
             </div>
             <div className={styles["user-info"]}>
-                <span className={styles["user-name"]}>{window.Telegram.WebApp.initDataUnsafe?.user?.first_name}</span>
-                <span className={styles["user-tag"]}>@{window.Telegram.WebApp.initDataUnsafe?.user?.username}</span>
+                <span className={styles["user-name"]}>{name}</span>
+                {username && <span className={styles["user-tag"]}>@{username}</span>}
             </div>
         </div>
     )
