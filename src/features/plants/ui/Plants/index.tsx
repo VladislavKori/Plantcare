cdd59b494403d14015daa7816f3d64ff26fa8773
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-03 23:16:22 +0500

    feat(ui): add HomePlants feature

---
+++ src/features/plants/ui/Plants/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Input } from "@shared/ui";
+import SearchIcon from "@shared/assets/search.svg?react"
+import { PlantList } from "@widgets/PlantList";
+import { plantList } from "./mock";
+
+export const HomePlants: FC = () => {
+    return (
+        <>
+            <Input placeholder="Search" startContent={<SearchIcon />} />
+            <PlantList data={plantList} />
+        </>
+    )
+}
---
+++ src/features/plants/ui/index.ts
@@ -0,0 +1,1 @@
+export { HomePlants } from "./Plants"
