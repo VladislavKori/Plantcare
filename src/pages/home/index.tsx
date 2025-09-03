2ea886d924b86f26f3b0197531d29fa47017f6ab
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-03 23:18:04 +0500

    feat(ui): add texts and HomePlants components on HomePage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
+import styles from "./style.module.scss";
+import { HomePlants } from "@features/plants/ui";
 
 export const HomePage: FC = () => {
     return (
-        <div>It's Home Page</div>
+        <>
+            <h2 className={styles["title"]}>Find interest plant!</h2>
+            <p className={styles["subtitle"]}>Discover and organize the flowers you grow at home, adding them to your collection to remember watering and learn up-to-date care tips.</p>
+            <HomePlants />
+        </>
     )
 }
