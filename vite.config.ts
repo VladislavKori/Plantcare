d4bfa464fd055836007789bfadc90cea70fdf4d6
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-03 23:09:57 +0500

    feat(libs&vite): add sass and vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
+import svgr from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "127.0.0.1"
+  },
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       {
