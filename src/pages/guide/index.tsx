f46ddc1884bbd41450cc142da10fa31a58d1f109
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-08 20:35:52 +0500

    feat(page): construct GuidePage

--- src/pages/guide/index.tsx
+++ src/pages/guide/index.tsx
@@ -1,7 +1,17 @@
 import { FC } from "react";
+import styles from "./style.module.scss";
+import { guideMock } from "./mock";
+import { Markdown } from "@widgets/Markdown";
+import { formatDate } from "@shared/utils/time";
 
 export const GuidePage: FC = () => {
     return (
-        <><h1>Guide page</h1></>
+        <>
+            <h2 className={styles["guide-name"]}>{guideMock.name}</h2>
+            <Markdown>
+                {guideMock.content}
+            </Markdown>
+            <p className={styles["guide-date"]}>Опубликовано: {formatDate(guideMock.created_at)}</p>
+        </>
     )
 }
