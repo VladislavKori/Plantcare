994b127d9219777b37538c65fe85e043678e7276
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-09 13:48:07 +0500

    feat(logic): add logic for GuidePage

--- src/pages/guide/index.tsx
+++ src/pages/guide/index.tsx
@@ -1,17 +1,36 @@
 import { FC } from "react";
 import styles from "./style.module.scss";
-import { guideMock } from "./mock";
 import { Markdown } from "@widgets/Markdown";
 import { formatDate } from "@shared/utils/time";
+import { useParams } from "react-router-dom";
+import { useDocument } from "react-firebase-hooks/firestore";
+import { doc } from "firebase/firestore";
+import { db } from "@shared/utils/firebase";
+import { Spinner } from "@shared/ui";
+import { BackHead } from "@widgets/BackHead";
 
 export const GuidePage: FC = () => {
+    const params = useParams<{ id: string }>()
+
+    const [value, loading, error] = useDocument(
+        doc(db, 'guides', `guide_${params.id}`),
+        {
+            snapshotListenOptions: { includeMetadataChanges: true },
+        }
+    );
+
+    if (loading) return <Spinner />
+    if (error) return <p>Ошибка</p>
+    if (value?.data() === undefined) return <p>Статья не найдена</p>
+
     return (
         <>
-            <h2 className={styles["guide-name"]}>{guideMock.name}</h2>
+            <BackHead />
+            <h2 className={styles["guide-name"]}>{value?.data()?.name}</h2>
             <Markdown>
-                {guideMock.content}
+                {value?.data()?.content}
             </Markdown>
-            <p className={styles["guide-date"]}>Опубликовано: {formatDate(guideMock.created_at)}</p>
+            <p className={styles["guide-date"]}>Опубликовано: {formatDate(value?.data()?.created_at)}</p>
         </>
     )
 }
