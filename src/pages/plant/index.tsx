721da586c952beb0207c1c60d3a3cb6bb67f5aeb
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-09 13:48:41 +0500

    feat(logic): connect PlantPage to firebase db

--- src/pages/plant/index.tsx
+++ src/pages/plant/index.tsx
@@ -1,11 +1,15 @@
 import { ImageSlider } from "@widgets/ImageSlider";
 import { FC } from "react";
 import { useParams } from "react-router-dom";
-import { plantMock } from "./mock";
 import { UsefulParams } from "@widgets/UsefulParams";
 import { Markdown } from "@widgets/Markdown";
 import styles from "./style.module.scss";
-import { Button } from "@shared/ui";
+import { Button, Spinner } from "@shared/ui";
+import { useDocument } from "react-firebase-hooks/firestore";
+import { doc } from "firebase/firestore";
+import { db } from "@shared/utils/firebase";
+import { BackHead } from "@widgets/BackHead";
+import { useCollectionStore } from "@entities/collection/model";
 
 export interface IPlantData {
     id: string;
@@ -23,22 +27,43 @@ export interface IPlantData {
 
 export const PlantPage: FC = () => {
     const params = useParams<{ id: string }>();
+    const collectionStore = useCollectionStore();
+
+    const [value, loading, error] = useDocument(
+        doc(db, 'plants', `plant_${params.id}`),
+        {
+            snapshotListenOptions: { includeMetadataChanges: true },
+        }
+    );
+
+    if (loading) return <Spinner />
+    if (error) return <p>Ошибка</p>
+    if (!params.id || value?.data() === undefined) return <p>Растение не найдено</p>
 
     return (
         <>
+            <BackHead />
             <ImageSlider
-                images={plantMock.imageURLs}
+                images={value?.data()?.imageURLs}
             />
             <UsefulParams
-                light={plantMock.params.light}
-                temp={plantMock.params.temp}
-                water={plantMock.params.water}
+                light={value?.data()?.params.light}
+                temp={value?.data()?.params.temp}
+                water={value?.data()?.params.water}
             />
-            <h2 className={styles["plant-name"]}>{plantMock.name}</h2>
-            <p className={styles["plant-description"]}>{plantMock.description}</p>
-            <Button fullWidth>Добавить в Коллекцию</Button>
+            <h2 className={styles["plant-name"]}>{value?.data()?.name}</h2>
+            <p className={styles["plant-description"]}>{value?.data()?.description}</p>
+            {collectionStore.plantIDExistInCollection(params.id) ? (
+                <Button fullWidth onClick={() => collectionStore.removeFromCollection(params.id as string)}>
+                    Удалить из коллекции
+                </Button>
+            ) : (
+                <Button fullWidth onClick={() => collectionStore.addToCollection(params.id as string)}>
+                    Добавить в Коллекцию
+                </Button>
+            )}
             <Markdown>
-                {plantMock.content}
+                {value?.data()?.content}
             </Markdown>
         </>
     )
