34498df044b7b96831c8b1f0aa692d51f48cf377
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-09 13:49:35 +0500

    feat(logic): connect PlantCard and PlantList to firebase db

--- src/widgets/PlantCard/index.tsx
+++ src/widgets/PlantCard/index.tsx
@@ -2,28 +2,36 @@ import { FC } from "react";
 import styles from "./style.module.scss";
 import { Button } from "@shared/ui";
 import { useNavigate } from "react-router-dom";
+import { IPlantData } from "@pages/plant";
+import { useCollectionStore } from "@entities/collection/model";
 
-export interface IPlantCard {
-    id: string;
-    name: string;
-    description: string;
-    previewURL: string;
-}
-
-export const PlantCard: FC<IPlantCard> = (props) => {
+export const PlantCard: FC<IPlantData> = (props) => {
     const navigate = useNavigate();
-    
+    const collectionStore = useCollectionStore();
+
     return (
         <div className={styles["card"]}>
             <div className={styles["card-image"]}>
-                <img src={props.previewURL} />
+                <img src={props.imageURLs[0]} />
             </div>
             <div className={styles["card-info"]}>
                 <h2 className={styles["card-name"]}>{props.name}</h2>
                 <p className={styles["card-text"]}>{props.description}</p>
                 <div className={styles["card-buttons"]}>
-                    <Button onClick={() => navigate(`/plant/${props.id}`)}>Read about</Button>
-                    <Button>Add to Collection</Button>
+                    <Button onClick={() => navigate(`/plant/${props.id}`)}>Подробнее</Button>
+                    {collectionStore.plantIDExistInCollection(props.id) ? (
+                        <Button
+                            onClick={() => collectionStore.removeFromCollection(props.id)}
+                        >
+                            Удалить
+                        </Button>
+                    ) : (
+                        <Button
+                            onClick={() => collectionStore.addToCollection(props.id)}
+                        >
+                            В Коллекцию
+                        </Button>
+                    )}
                 </div>
             </div>
         </div>
--- src/widgets/PlantList/index.tsx
+++ src/widgets/PlantList/index.tsx
@@ -1,14 +1,18 @@
-import { IPlantCard, PlantCard } from "@widgets/PlantCard";
+import { PlantCard } from "@widgets/PlantCard";
 import { FC } from "react";
 import styles from "./style.module.scss";
+import { IPlantData } from "@pages/plant";
 
 export interface IPlantListProps {
-    data: IPlantCard[]
+    data: IPlantData[]
 }
 
 export const PlantList: FC<IPlantListProps> = (props) => {
     return (
         <div className={styles["list"]}>
+            {props.data.length === 0 && (
+                <p>Ничего не найдено</p>
+            )}
             {props.data.map(item => (
                 <PlantCard {...item} />
             ))}
