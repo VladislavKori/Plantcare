e163e76870bb0d856efb3bf4f633fac36f733f37
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-18 06:48:03 +0500

    feat(ui): Update BackHead design

--- src/widgets/BackHead/index.tsx
+++ src/widgets/BackHead/index.tsx
@@ -1,13 +1,42 @@
-import { Button } from "@shared/ui";
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./style.module.scss";
+import ArrowLeftIcon from "@shared/assets/icons/arrow.svg?react";
+import PlusIcon from "@shared/assets/icons/plus.svg?react";
+import MinusIcon from "@shared/assets/icons/minus.svg?react";
+import { useCollectionStore } from "@entities/collection/model";
 
-export const BackHead: FC = () => {
+interface BackHeadProps {
+    id: string;
+    name: string;
+}
+
+export const BackHead: FC<BackHeadProps> = (props) => {
     const navigate = useNavigate();
+    const collectionStore = useCollectionStore();
 
     return (
-        <div>
-            <Button onClick={() => navigate(-1)}>Назад</Button>
+        <div className={styles["head"]}>
+            <button onClick={() => navigate(-1)} className={styles["head-button"]}>
+                <ArrowLeftIcon />
+            </button>
+            <p className={styles["head-name"]}>{props.name}</p>
+            {collectionStore.plantIDExistInCollection(props.id) ? (
+                <button
+                    className={styles["head-button"]}
+                    onClick={() => collectionStore.removeFromCollection(props.id as string)}
+                >
+                    <MinusIcon />
+                </button>
+            ) : (
+                <button
+                    className={styles["head-button"]}
+                    onClick={() => collectionStore.addToCollection(props.id as string)}
+                >
+                    <PlusIcon />
+                </button>
+            )}
+
         </div>
     )
 }
