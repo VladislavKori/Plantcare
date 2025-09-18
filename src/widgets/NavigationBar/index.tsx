de275d4317b370d65aabb0c4abddf2c024db8582
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-18 06:44:14 +0500

    feat(ui): change navbar style

--- src/widgets/NavigationBar/index.tsx
+++ src/widgets/NavigationBar/index.tsx
@@ -2,34 +2,39 @@ import { FC } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./style.module.scss";
 import clsx from "clsx";
+import PlantIcon from "@shared/assets/icons/plant.svg?react";
+import HomeIcon from "@shared/assets/icons/home.svg?react";
+import BookIcon from "@shared/assets/icons/book.svg?react";
 
 export const NavigationBar: FC = () => {
     return (
-        <div className={styles["nav"]}>
-            <NavLink
-                className={({ isActive }) =>
-                    isActive ? clsx(styles["nav-link"], styles["nav-link-active"]) : styles["nav-link"]
-                }
-                to="/guides"
-            >
-                Статьи
-            </NavLink>
-            <NavLink
-                className={({ isActive }) =>
-                    isActive ? clsx(styles["nav-link"], styles["nav-link-active"]) : styles["nav-link"]
-                }
-                to="/"
-            >
-                Растения
-            </NavLink>
-            <NavLink
-                className={({ isActive }) =>
-                    isActive ? clsx(styles["nav-link"], styles["nav-link-active"]) : styles["nav-link"]
-                }
-                to="/collection"
-            >
-                Коллекция
-            </NavLink>
+        <div className={styles["nav-wrapper"]}>
+            <div className={styles["nav"]}>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? clsx(styles["nav-link"], styles["nav-link-active"]) : styles["nav-link"]
+                    }
+                    to="/guides"
+                >
+                    <BookIcon />
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? clsx(styles["nav-link"], styles["nav-link-active"]) : styles["nav-link"]
+                    }
+                    to="/"
+                >
+                    <PlantIcon />
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? clsx(styles["nav-link"], styles["nav-link-active"]) : styles["nav-link"]
+                    }
+                    to="/collection"
+                >
+                    <HomeIcon />
+                </NavLink>
+            </div>
         </div>
     )
 }
