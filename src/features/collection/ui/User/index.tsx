84b82cb3e7f5c76bc995fae04511979966e3545a
Vladislav Kori <vladislav.kori.work@gmail.com>
2025-09-21 15:30:25 +0500

    feat(ui): update styles for User component

--- src/features/collection/ui/User/index.tsx
+++ src/features/collection/ui/User/index.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import styles from "./style.module.scss";
+import SettingIcon from "@shared/assets/icons/setting.svg?react";
+import { Link } from "react-router-dom";
 
 export const User: FC = () => {
     const photoURL = window.Telegram.WebApp.initDataUnsafe?.user?.photo_url
@@ -8,12 +10,19 @@ export const User: FC = () => {
 
     return (
         <div className={styles["user"]}>
-            <div className={styles["user-avatar"]}>
-                <img src={photoURL} alt="user" />
+            <div className={styles["user-left"]}>
+                <div className={styles["user-avatar"]}>
+                    <img src={photoURL} alt="user" />
+                </div>
+                <div className={styles["user-info"]}>
+                    <span className={styles["user-name"]}>{name}</span>
+                    {username && <span className={styles["user-tag"]}>@{username}</span>}
+                </div>
             </div>
-            <div className={styles["user-info"]}>
-                <span className={styles["user-name"]}>{name}</span>
-                {username && <span className={styles["user-tag"]}>@{username}</span>}
+            <div className={styles["user-right"]}>
+                <Link to="/settings" className={styles["user-settings"]}>
+                    <SettingIcon />
+                </Link>
             </div>
         </div>
     )
